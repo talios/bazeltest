9cef67973f9c28e433c5ee5462c47388d514b6bf
Mark Derricutt <mark.derricutt@smxemail.com>
2016-02-11 13:29:12 +1300

    Initial

---
+++ WORKSPACE
@@ -0,0 +1,7 @@
+maven_jar(name = "guava",            artifact = "com.google.guava:guava:19.0")
+maven_jar(name = "dagger",           artifact = "com.google.dagger:dagger:2.0")
+maven_jar(name = "dagger_compiler",  artifact = "com.google.dagger:dagger-compiler:2.0")
+maven_jar(name = "dagger_producers", artifact = "com.google.dagger:dagger-producers:2.0-beta")
+maven_jar(name = "javax_inject",     artifact = "javax.inject:javax.inject:1")
+maven_jar(name = "javaslang",        artifact = "com.javaslang:javaslang:1.2.0")
+maven_jar(name = "javapoet",         artifact = "com.squareup:javapoet:1.0.0")
---
+++ src/main/java/com/talios/bazeltest/Main.java
@@ -0,0 +1,50 @@
+package com.talios.bazeltest;
+
+//import javaslang.collection.List;
+//import javaslang.control.Try;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    
+//    Try<List<String>> vals = Try.of(() -> List.of(new String[] {"one", "two", "three"}));
+
+//    vals.forEach(vargs -> System.out.println(vargs.get(1)));
+
+    System.out.println("Hello world.");
+    
+    
+//    GreetingComponent greeting = DaggerGreetingComponent.create();
+    
+//    System.out.println(greeting.provideGreeter().greet("Mark"));
+        
+
+    // code-gen
+    
+//    MethodSpec main = MethodSpec.methodBuilder("main")
+//        .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+//        .returns(void.class)
+//        .addParameter(String[].class, "args")
+//        .addJavadoc("// hello - generated at $L", new java.util.Date().toString())
+//        .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+//        .build();
+//
+//    TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+//        .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+//        .addMethod(main)
+//        .build();
+//
+//    JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+//        .build();
+//
+//
+//    Try.of(() -> {
+//      javaFile.writeTo(System.out);
+//      return null;
+//    });
+//
+
+
+  }  
+  
+}
